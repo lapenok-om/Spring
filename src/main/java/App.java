231f52d2746e18ecc964b0d75000a1ce379a50d3
Lapenok <osharapova@yandex.com>
2022-12-17 08:57:05 +0300

    bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean1);
+        System.out.println(bean);
+        System.out.println(bean1);
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(cat1 == cat2);
+        System.out.println(cat1);
+        System.out.println(cat2);
+
     }
 }
